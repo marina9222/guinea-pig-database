f1b228c6cf12817fd13a9b4d3cc87c5a20614ba2
Marina Kostova <marinakostova9222@gmail.com>
2024-06-23 12:11:43 +0000

    Add: pets template and fixed mongo_uri string

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from functools import wraps
-from flask import Flask
+from flask import (
+Flask,flash,render_template,
+redirect,request,session,url_for)
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -17,6 +19,8 @@ app = Flask(__name__)
 # Configurations for MONGO DB
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
@@ -30,11 +34,15 @@ cloudinary.config(
 
 
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/get_pets")
+def get_pets():
+    pets = mongo.db.pets.find()
+    return render_template('pets.html', pets=pets)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
+            
