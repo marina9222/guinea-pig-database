ae2703273da15159196a47bad079cbd4baae9867
Marina Kostova <marinakostova9222@gmail.com>
2024-06-23 14:05:15 +0000

    Add: Materialize navbar , side nav, media query for the nav bar

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = {
+        edge: 'right',
+    }
+
+    var instances = M.Sidenav.init(elems, options);
+  });
