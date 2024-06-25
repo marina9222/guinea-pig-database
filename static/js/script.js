6971acee741d0a0cb17d774e3877faaabb52e78b
Marina Kostova <marinakostova9222@gmail.com>
2024-06-25 09:44:16 +0000

    Add: Pet collapsible menu

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var instances = M.Sidenav.init(elems, options);
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    
+    var instances = M.Collapsible.init(elems, {
+      
+      });
+      
+    
+  });
+          
